7abc552b74ca47cad95ddcea4ab36d5923655cc3
yujin-zero <poly30@naver.com>
2025-01-21 22:06:29 +0900

    feat: SecurityFilterChain을 통해 CSRF 비활성화

--- backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() { // 인터페이스
         return new BCryptPasswordEncoder(); // 구현체
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 새로운 방식으로 CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
 }
