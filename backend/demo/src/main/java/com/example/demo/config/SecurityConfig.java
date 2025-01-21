09cc9a7b5957b0cea6c847e3676be117780f4497
yujin-zero <poly30@naver.com>
2025-01-21 20:44:45 +0900

    feat: PasswordEncoder 빈 등록을 위한 SecurityConfig 추가

--- backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,21 +1,15 @@
-//package com.example.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .anyRequest().permitAll() // 모든 요청 허용
-//                )
-//                .csrf(csrf -> csrf.disable()); // CSRF 비활성화
-//
-//        return http.build();
-//    }
-//}
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { // 인터페이스
+        return new BCryptPasswordEncoder(); // 구현체
+    }
+}
